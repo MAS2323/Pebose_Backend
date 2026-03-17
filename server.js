ea419bf472e6ad13e1a9870456aa4c274d31f89b
EL MAS <onewemas2323@gmail.com>
2026-03-18 00:51:20 +0100

    primer commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ import { protect } from "./middleware/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+app.use(cors());
 // 🛡️ Security middleware
 app.use(
   helmet({
